429306e847f8e93e796c8b058bf1ab44daf3d41f
Steerpike <imthekoul@gmail.com>
2022-10-20 03:04:36 +0300

    Dockerfile && layout

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:19-alpine
+
+RUN apk add --no-cache tini
+
+EXPOSE 300
+
+WORKDIR /usr/src/app
+
+COPY package*.json ./
+
+RUN npm install && npm cache clean --force
+
+COPY . .
+
+CMD [ "/sbin/tini","--","node","/bin/www" ]
--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const app = express();
 
 //Setup View Engine
 
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
